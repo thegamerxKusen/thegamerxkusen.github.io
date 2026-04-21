74aac2fc3fa96760b056b74f9117ea9925fee598
thegamerxKusen <lacazze.alexandre@gmail.com>
2026-04-21 12:58:24 +0200

    created footwork

--- javascript/technique.js
+++ javascript/technique.js
@@ -18,19 +18,21 @@ const breathing_tech_db = {
     }
 
 class FOOTWORK_TECHNIQUE {
-    constructor(name, desc, speed_boost, tier) {
+    constructor(name, description, activeCost, passiveSpeed, activeLogic) {
         this.name = name
-        this.desc = desc
-        this.speed_boost = speed_boost
-        this.tier = tier
+        this.description = description
+        this.activeCost = activeCost // How much Qi it costs to use the Evade
+        this.passiveSpeed = passiveSpeed
+        this.fightUse = activeLogic     // A function
     }
 
     fightUse(user, target) {
 
     }
 }
-const FOOTWORK_TECHNIQUE_DB = {
 
-}
-
-//basic_qi_tech : new BREATHING_TECHNIQUE("Basic Qi Technique","A rough way to gather energy, can barely be called a technique, you just take all energy letting it wild in your meridian until some, gather in your dantian by chance for refining.",1000,4,1,"Common",0),
+const footwork_tech_db = {
+    basic_evade : new FOOTWORK_TECHNIQUE("Basic Evade","A simple footwork technique that allows you to evade attacks, but it is not very effective.", 20, 1, (user, target) => {
+        sendConsoleMessage(`${user.name} uses ${footwork_tech_db.basic_evade.name} to evade the attack!`)})
+        //no speed boost just trying to evade, so no change to user.speed
+    }
