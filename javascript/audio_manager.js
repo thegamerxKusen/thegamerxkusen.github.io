efb9e720cb0028855e8148ed144cba164ffbecc8
thegamerxKusen <lacazze.alexandre@gmail.com>
2026-04-24 02:26:42 +0200

    added sound effect

--- javascript/audio_manager.js
+++ javascript/audio_manager.js
@@ -1,7 +1,6 @@
 class AudioManager {
     constructor() {
-        // --- 1. Background Music Setup ---
-        // Replace with the actual paths to your audio files!
+       
         this.calm_bgm = new Audio("assets/sounds/music/Lotus Pond - Loop.wav")
         
         this.calm_bgm.loop = true 
@@ -10,26 +9,22 @@ class AudioManager {
         this.fight_bgm = new Audio("assets/sounds/music/Dragon Dance - Loop.wav")
         this.fight_bgm.loop = true 
         this.fight_bgm.volume = 0.1
-        // --- 2. Preload Sound Effects ---
-        // Preloading them ensures there is no delay when you try to play them in combat
+        
         this.sfx = {
             //Ex: click: new Audio("assets/sounds/ui_click.wav"),
         };
 
-        // Set specific volumes for SFX if needed
+        
     }
 
-    // --- Play Background Music ---
     playCalmBGM(){
         this.stopFightBGM()
-        // The .catch() prevents the game from crashing if the browser blocks autoplay
         this.calm_bgm.play().catch(error => {
             console.log("Browser blocked autoplay. Waiting for user interaction.");
         });
     }
     playFightBGM(){
         this.stopCalmBGM()
-        // The .catch() prevents the game from crashing if the browser blocks autoplay
         this.fight_bgm.play().catch(error => {
             console.log("Browser blocked autoplay. Waiting for user interaction.");
         });
@@ -37,20 +32,16 @@ class AudioManager {
 
     stopCalmBGM() {
         this.calm_bgm.pause();
-        this.calm_bgm.currentTime = 0; // Rewind to the start
     }
 
     stopFightBGM() {
         this.calm_bgm.pause();
-        this.calm_bgm.currentTime = 0; // Rewind to the start
+        
     }
 
     // --- Play Sound Effect ---
     playSFX(soundName) {
         if (this.sfx[soundName]) {
-            // THE SECRET TRICK: 
-            // If you click a button 3 times fast, standard play() ignores clicks 2 and 3 because it's already playing.
-            // Resetting currentTime to 0 forces it to restart instantly so it feels responsive!
             this.sfx[soundName].currentTime = 0; 
             this.sfx[soundName].play();
         } else {
@@ -59,5 +50,5 @@ class AudioManager {
     }
 }
 
-// Instantiate it globally so all your files can use it!
+
 const gameAudio = new AudioManager();
